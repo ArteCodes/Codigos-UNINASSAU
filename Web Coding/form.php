1e2f9ad0cda14f0190ce4d13ab8509826427fe57
Edson <edsonsoliveirajunior@gmail.com>
2024-09-21 22:22:58 -0300

    Parte php da atividade em sala

---
+++ Web Coding/form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>segunda pagina</title>
+</head>
+<body>
+
+<?php
+  $nome = isset($_GET["nome"])?$_GET["nome"]:"nome não informado";
+  $email = isset($_GET["email"])?$_GET["email"]:"email não informado";
+  $sexo = isset($_GET["sexo"])?$_GET["sexo"]:"Sexo não informado";
+  echo ("<h2>Nome: $nome</h2></br>");
+
+  echo ("<h2>Email: $email</h2></br>");
+
+  echo ("<h2>Sexo: $sexo</h2></br>");
+?>
+
+</body>
+</html>
